39745267757800161cc101a5df91ecc795c27444
Yazmin Gutierrez Hernandez <230447@utxicotepec.edu.mx>
2024-09-17 15:23:32 -0600

    Practica 04 Ejercicio 03
    Agregacion de los tipos de datos :String ,BigInt ,Symboly y Null

---
+++ JS/Ejercicio3.js
@@ -0,0 +1,29 @@
+// strings o cadenas de texto
+// const alumno = "juan";
+// let producto ="monitor 20 pulgadas";
+// const numero ="30";
+// const nmero2 =30;
+// console.log(typeof numero)
+
+
+ //BigINT
+// const  numeroGrande = BigInt(43246466566)
+// console.log(typeof numeroGrande);
+// const num =23;
+// const num2 =13;
+// console.log(num + Number(numeroGrande)) ;
+
+
+
+ //SYMBOL
+// const primerSymbol =Symbol (39);
+// const segundoSymbol =Symbol (39);
+
+// console.log(primerSymbol == segundoSymbol);
+// console.log(primerSymbol.valueOf());
+// console.log(segundoSymbol.valueOf());
+
+
+//NULL
+// const  descuento =null;
+// console.log(typeof descuento);
